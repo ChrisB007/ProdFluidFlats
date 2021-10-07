c22158dfd158faa5cb0e56b9ca1565bfcff298a1
ChrisB007 <chrisbitoy@gmail.com>
2021-10-07 18:31:41 -0400

    removed dynamic email

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -38,7 +38,7 @@ function Signin() {
                           className="hideus inputField appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                           type="email"
                           placeholder="Your email"
-                          value={email}
+                          value=""
                           onChange={(e) => setEmail(e.target.value)}
                         />
                       </div>
